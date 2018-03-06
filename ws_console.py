583bebc3bdfe23c23e50a0adb52598bb743c908a
y-du <42994541+y-du@users.noreply.github.com>
2018-03-06 13:58:44 +0100

    subprocess test

--- ws_console.py
+++ ws_console.py
@@ -1,37 +1,66 @@
 try:
     import websockets
     from modules.logger import root_logger
-    from device_controller import OUT_QUEUE
 except ImportError as ex:
     exit("{} - {}".format(__name__, ex.msg))
 
 from queue import Empty
 from threading import Thread
-import concurrent.futures, functools, asyncio
+import concurrent.futures, functools, asyncio, subprocess, select
 
 logger = root_logger.getChild(__name__)
 
 class WebsocketConsole(Thread):
+    _source = None
+
     def __init__(self):
         super().__init__()
         self.start()
 
+    @staticmethod
+    def setSource(src):
+        __class__._source = src
+
     async def send(self, websocket, path):
-        OUT_QUEUE.queue.clear()
-        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-            while True:
+        while True:
+            logger.info("outer w")
+            if __class__._source:
+                tail_process = subprocess.Popen(['tail', '-F', __class__._source], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                poll_obj = select.poll()
+                poll_obj.register(tail_process.stdout)
+                def readTail():
+                    logger.info(poll_obj.poll(0))
+                    if poll_obj.poll(1):
+                        line = tail_process.stdout.readline().decode()
+                        return line.replace('\n', '').replace('\r', '')
+                    else:
+                        tail_process.kill()
+                with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+                    while True:
+                        logger.info("inner w")
+                        payload = await self._event_loop.run_in_executor(
+                            executor,
+                            readTail
+                        )
+                        logger.info(payload)
+                        if payload:
+                            try:
+                                await websocket.send(payload)
+                            except Exception as ex:
+                                logger.warning("could not send data - {}".format(ex))
+                                break
+                        else:
+                            break
+                break
+            else:
                 try:
-                    payload = await self._event_loop.run_in_executor(
-                        executor,
-                        functools.partial(OUT_QUEUE.get, timeout=1)
-                    )
-                    try:
-                        await websocket.send(payload)
-                    except Exception as ex:
-                        logger.warning("could not send data - {}".format(ex))
-                        break
-                except Empty:
-                    pass
+                    await websocket.ping()
+                    await asyncio.sleep(1)
+                except Exception as ex:
+                    logger.warning(ex)
+                    break
+        __class__._source = None
+        logger.info('#################')
 
     def run(self):
         try:
