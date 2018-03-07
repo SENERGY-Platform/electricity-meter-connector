30a642ec0342e1455127837bb82d9426105208c9
y-du <42994541+y-du@users.noreply.github.com>
2018-03-07 08:40:18 +0100

    added try blocks to avoid exceptions with missing controllers

--- web_gui.py
+++ web_gui.py
@@ -21,14 +21,6 @@ class WebGUI(Thread):
         self._port = 8000
         self.start()
 
-    @staticmethod
-    @app.route('/test')
-    def test():
-        def liDevices():
-            for device in SerialManager.getDevices():
-                yield '<a href="{}">{}</a></br>'.format(device, device)
-        return Response(liDevices())
-
     def run(self):
         __class__.app.run(host=self._host, port=self._port)
 
@@ -44,44 +36,68 @@ class WebGUI(Thread):
     def device(d_id):
         devices = SerialManager.getDevices()
         devices.sort()
-        controller = SerialManager.getController(d_id)
-        if controller:
-            WebsocketConsole.setSource(controller.log_file)
-        return render_template('gui.html', devices=devices, d_id=d_id)
+        try:
+            controller = SerialManager.getController(d_id)
+            if controller:
+                WebsocketConsole.setSource(controller.log_file)
+            return render_template('gui.html', devices=devices, d_id=d_id)
+        except Exception as ex:
+            logger.error(ex)
+        return render_template('gui.html', devices=devices)
 
     @staticmethod
     @app.route('/<d_id>/<end_point>', methods=['POST'])
     def endpoint(d_id, end_point):
         devices = SerialManager.getDevices()
-        controller = SerialManager.getController(d_id)
-        if controller and d_id in devices:
-            if end_point == "mr":
-                controller.manualRead()
-                return Response(status=200)
-            if end_point == "strt":
-                controller.startDetection()
-                return Response(status=200)
-            if end_point == "stp":
-                controller.stopAction()
-                return Response(status=200)
-            if end_point == "eas":
-                controller.enableAutoStart()
-                return Response(status=200)
-            if end_point == "das":
-                controller.disableAutoStart()
-                return Response(status=200)
-            if end_point == "res":
-                controller.haltController()
-                return Response(status=200)
+        try:
+            controller = SerialManager.getController(d_id)
+            if controller and d_id in devices:
+                if end_point == "mr":
+                    controller.manualRead()
+                    return Response(status=200)
+                if end_point == "strt":
+                    controller.startDetection()
+                    return Response(status=200)
+                if end_point == "stp":
+                    controller.stopAction()
+                    return Response(status=200)
+                if end_point == "eas":
+                    controller.enableAutoStart()
+                    return Response(status=200)
+                if end_point == "das":
+                    controller.disableAutoStart()
+                    return Response(status=200)
+                if end_point == "res":
+                    controller.haltController()
+                    return Response(status=200)
+        except Exception as ex:
+            logger.error(ex)
         return Response(status=500)
 
     @staticmethod
     @app.route('/<d_id>/conf', methods=['POST'])
     def conf(d_id):
         devices = SerialManager.getDevices()
-        controller = SerialManager.getController(d_id)
-        nat, dt, lld = request.form['nat'], request.form['dt'], request.form['lld']
-        if controller and d_id in devices:
-            controller.configureDevice(nat, dt, lld)
-            return Response(status=200)
+        try:
+            controller = SerialManager.getController(d_id)
+            nat, dt, lld = request.form['nat'], request.form['dt'], request.form['lld']
+            if controller and d_id in devices:
+                controller.configureDevice(nat, dt, lld)
+                return Response(status=200)
+        except Exception as ex:
+            logger.error(ex)
+        return Response(status=500)
+
+    @staticmethod
+    @app.route('/<d_id>/rot', methods=['POST'])
+    def rpkwh(d_id):
+        devices = SerialManager.getDevices()
+        try:
+            controller = SerialManager.getController(d_id)
+            rot = request.form['nat']
+            if controller and d_id in devices:
+                controller.configureDevice()
+                return Response(status=200)
+        except Exception as ex:
+            logger.error(ex)
         return Response(status=500)
