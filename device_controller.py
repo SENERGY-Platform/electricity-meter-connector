aea820483920a20ead3663165495ea6a661ab02b
y-du <42994541+y-du@users.noreply.github.com>
2018-05-22 13:01:31 +0200

    fixed autostart bug

--- device_controller.py
+++ device_controller.py
@@ -318,12 +318,12 @@ class DeviceController(Thread):
             self._writeToOutput('rotations/kWh not set')
 
     def enableAutoStart(self):
-        devices_db.updateDeviceConf(self._dip_id, strt=1)
-        self._strt = 1
+        if devices_db.updateDeviceConf(self._dip_id, strt=1):
+            self._strt = 1
 
     def disableAutoStart(self):
-        devices_db.updateDeviceConf(self._dip_id, strt=0)
-        self._strt = 0
+        if devices_db.updateDeviceConf(self._dip_id, strt=0):
+            self._strt = 0
 
     def haltController(self):
         self._commands.put(self._haltController)
