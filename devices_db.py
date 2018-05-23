93a3a7b7e925190a398dfdd2aee2f54f068f7bce
y-du <42994541+y-du@users.noreply.github.com>
2018-05-23 17:03:07 +0200

    rewrite

--- devices_db.py
+++ devices_db.py
@@ -12,52 +12,38 @@ logger = root_logger.getChild(__name__)
 class DevicesDatabase(metaclass=Singleton):
     _db_path = '{}/devices.sqlite'.format(os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0])))
     _devices_table = 'devices'
-    _id_field = ('id', 'TEXT')
-    _nat_field = ('nat', 'INTEGER')
-    _dt_field = ('dt', 'INTEGER')
-    _ndt_field = ('ndt', 'INTEGER')
-    _strt_field = ('strt', 'INTEGER')
-    _rpkwh_field = ('rpkwh', 'INTEGER')
-    _lld_field = ('lld', 'INTEGER')
-    _kWh_field = ('kWh', 'TEXT')
-    _name_field = ('name', 'TEXT')
+    _devices_col = [
+        ('id', 'TEXT PRIMARY KEY'),
+        ('nat', 'INTEGER DEFAULT 9000'),
+        ('lld', 'INTEGER DEFAULT 20'),
+        ('lb', 'INTEGER'),
+        ('rb', 'INTEGER'),
+        ('dt', 'INTEGER DEFAULT 50'),
+        ('ndt', 'INTEGER DEFAULT 150'),
+        ('strt', 'INTEGER DEFAULT 0'),
+        ('rpkwh', 'INTEGER DEFAULT 0'),
+        ('kwh', 'TEXT DEFAULT "0.0"'),
+        ('name', 'TEXT'),
+        ('mode', 'TEXT DEFAULT "H"')
+    ]
     _sm_conf_table = 'sm_conf'
-    _id_prefix_field = ('id_prefix', 'TEXT')
+    _sm_conf_col = [
+        ('id_prefix', 'TEXT')
+    ]
 
     def __init__(self):
         if not os.path.isfile(__class__._db_path):
             logger.debug('no database found')
-            query = 'CREATE TABLE {table} ({id} {id_t} PRIMARY KEY, {nat} {nat_t} DEFAULT 9000, {dt} {dt_t} DEFAULT 100, {ndt} {ndt_t} DEFAULT 200, {strt} {strt_t} DEFAULT 0, {rpkwh} {rpkwh_t} DEFAULT 0, {lld} {lld_t} DEFAULT 20, {kWh} {kWh_t} DEFAULT 0.0, {name} {name_t})'.format(
-                table=__class__._devices_table,
-                id=__class__._id_field[0],
-                id_t=__class__._id_field[1],
-                nat=__class__._nat_field[0],
-                nat_t=__class__._nat_field[1],
-                dt=__class__._dt_field[0],
-                dt_t=__class__._dt_field[1],
-                ndt=__class__._ndt_field[0],
-                ndt_t=__class__._ndt_field[1],
-                strt=__class__._strt_field[0],
-                strt_t=__class__._strt_field[1],
-                rpkwh=__class__._rpkwh_field[0],
-                rpkwh_t=__class__._rpkwh_field[1],
-                lld=__class__._lld_field[0],
-                lld_t=__class__._lld_field[1],
-                kWh=__class__._kWh_field[0],
-                kWh_t=__class__._kWh_field[1],
-                name=__class__._name_field[0],
-                name_t=__class__._name_field[1]
-            )
+            flatten = lambda li: [' '.join(map(str, sublist)) for sublist in li]
+            columns = ', '.join(map(str, flatten(__class__._devices_col)))
+            query = 'CREATE TABLE {} ({})'.format(__class__._devices_table, columns)
             self._executeQuery(query)
-            query = 'CREATE TABLE {table} ({id_prefix} {id_prefix_t})'.format(
-                table=__class__._sm_conf_table,
-                id_prefix=__class__._id_prefix_field[0],
-                id_prefix_t=__class__._id_prefix_field[1]
-            )
+            columns = ', '.join(map(str, flatten(__class__._sm_conf_col)))
+            query = 'CREATE TABLE {} ({})'.format(__class__._sm_conf_table, columns)
             self._executeQuery(query)
             query = 'INSERT INTO {table} ({id_prefix}) VALUES ("{id_prefix_v}")'.format(
                 table=__class__._sm_conf_table,
-                id_prefix=__class__._id_prefix_field[0],
+                id_prefix=__class__._sm_conf_col[0][0],
                 id_prefix_v=str(uuid4())
             )
             self._executeQuery(query)
@@ -66,15 +52,17 @@ class DevicesDatabase(metaclass=Singleton):
             logger.debug("found database at '{}'".format(__class__._db_path))
 
     def _executeQuery(self, query):
+        logger.info(query)
         try:
             db_conn = sqlite3.connect(__class__._db_path)
+            db_conn.row_factory = sqlite3.Row
             cursor = db_conn.cursor()
             cursor.execute(query)
             if any(statement in query for statement in ('CREATE', 'INSERT', 'DELETE', 'UPDATE')):
                 db_conn.commit()
                 result = True
             else:
-                result = cursor.fetchall()
+                result = cursor.fetchone()
             db_conn.close()
             return result
         except Exception as ex:
@@ -84,63 +72,42 @@ class DevicesDatabase(metaclass=Singleton):
     def addDevice(self, device_id):
         query = 'INSERT INTO {table} ({id}) VALUES ("{id_v}")'.format(
             table=__class__._devices_table,
-            id=__class__._id_field[0],
+            id=__class__._devices_col[0][0],
             id_v=device_id,
         )
         return self._executeQuery(query)
 
     def getDeviceConf(self, device_id):
-        query = 'SELECT {nat}, {dt}, {ndt}, {strt}, {rpkwh}, {lld}, {kWh}, {name} FROM {table} WHERE {id}="{id_v}"'.format(
+        #firstElement = lambda li : [sublist[0] for sublist in li]
+        #columns = ', '.join(map(str, firstElement(__class__._devices_col)[1:]))
+        query = 'SELECT * FROM {table} WHERE {id}="{id_v}"'.format(
+            #col=columns,
             table=__class__._devices_table,
-            nat=__class__._nat_field[0],
-            dt=__class__._dt_field[0],
-            ndt=__class__._ndt_field[0],
-            strt=__class__._strt_field[0],
-            rpkwh=__class__._rpkwh_field[0],
-            lld=__class__._lld_field[0],
-            kWh=__class__._kWh_field[0],
-            name=__class__._name_field[0],
-            id=__class__._id_field[0],
+            id=__class__._devices_col[0][0],
             id_v=device_id
         )
         result = self._executeQuery(query)
+        logger.info(result)
         if result:
-            return {
-                'nat': result[0][0],
-                'dt': result[0][1],
-                'ndt': result[0][2],
-                'strt': result[0][3],
-                'rpkwh': result[0][4],
-                'lld': result[0][5],
-                'kWh': result[0][6],
-                'name': result[0][7]
-            }
+            r_dict = dict()
+            for key in result.keys():
+                r_dict[key] = result[key]
+            return r_dict
         return None
 
-    def updateDeviceConf(self, device_id, nat=None, dt=None, ndt=None, strt=None, rpkwh=None, lld=None, kWh=None, name=None):
+    def updateDeviceConf(self, device_id, **kwargs):
+        firstElement = lambda li: [sublist[0] for sublist in li]
+        columns = firstElement(__class__._devices_col)
         values = list()
-        if nat:
-            values.append('{}={}'.format(__class__._nat_field[0], nat))
-        if dt:
-            values.append('{}={}'.format(__class__._dt_field[0], dt))
-        if ndt:
-            values.append('{}={}'.format(__class__._ndt_field[0], ndt))
-        if not strt == None:
-            values.append('{}={}'.format(__class__._strt_field[0], strt))
-        if rpkwh:
-            values.append('{}={}'.format(__class__._rpkwh_field[0], rpkwh))
-        if lld:
-            values.append('{}={}'.format(__class__._lld_field[0], lld))
-        if kWh:
-            values.append('{}="{}"'.format(__class__._kWh_field[0], str(kWh)))
-        if name:
-            values.append('{}="{}"'.format(__class__._name_field[0], str(name)))
+        for arg in kwargs:
+            if arg in columns:
+                values.append('{}={}'.format(arg, kwargs[arg]))
         values = ', '.join(map(str, values))
         if values:
             query = 'UPDATE {table} SET {values} WHERE {id}="{id_v}"'.format(
                 table=__class__._devices_table,
                 values=values,
-                id=__class__._id_field[0],
+                id=__class__._devices_col[0][0],
                 id_v=device_id
             )
             return self._executeQuery(query)
@@ -149,9 +116,9 @@ class DevicesDatabase(metaclass=Singleton):
     def getIdPrefix(self):
         query = 'SELECT {id_prefix} FROM {table} WHERE _rowid_="1"'.format(
             table=__class__._sm_conf_table,
-            id_prefix=__class__._id_prefix_field[0]
+            id_prefix=__class__._sm_conf_col[0][0]
         )
         result = self._executeQuery(query)
         if result:
-            return result[0][0]
+            return result['id']
         return None
