fa0c8a032dbceef3d2a3efbb921d51fe9bb0ec81
y-du <42994541+y-du@users.noreply.github.com>
2018-07-12 23:34:32 +0200

    push readings on a 20s interval

--- client.py
+++ client.py
@@ -6,16 +6,40 @@ try:
     from web_ui.app import WebUI
 except ImportError as ex:
     exit("{} - {}".format(__name__, ex.msg))
-import asyncio
+from threading import Thread
+import asyncio, time, json, datetime
 
+
+def pushReadings():
+    while True:
+        for controller in SerialManager.getControllers():
+            try:
+                if controller._kwh:
+                    Client.event(
+                        controller._extended_id,
+                        'detection',
+                        json.dumps({
+                            'value': controller._kwh,
+                            'unit': 'kWh',
+                            'time': '{}Z'.format(datetime.datetime.utcnow().isoformat())
+                        }),
+                        block=False
+                    )
+                    time.sleep(0.1)
+            except Exception:
+                pass
+        time.sleep(20)
+
+readings_scraper = Thread(target=pushReadings, name="Scraper")
 
 loop = asyncio.get_event_loop()
 cw = asyncio.get_child_watcher()
 
 
 if __name__ == '__main__':
-    connector_client = Client(device_manager=DevicePool)
+    #connector_client = Client(device_manager=DevicePool)
     WebsocketConsole(loop)
     WebUI()
     SerialManager()
+    readings_scraper.start()
     loop.run_forever()
